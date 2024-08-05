f70b2082d11181f81db30c3ff3520075bce744fa
hanyue1014 <47289292+hanyue1014@users.noreply.github.com>
2024-08-05 17:26:09 +0800

    Add initial grpc setup

---
+++ SimulationServicer.py
@@ -0,0 +1,5 @@
+import MarCom_Proto.python.simulation_pb2_grpc as simulation_pb2_grpc
+import MarCom_Proto.python.simulation_pb2 as simulation_pb2
+
+class SimulationServicer(simulation_pb2_grpc):
+    pass
