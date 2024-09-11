ad3e4b1c0bb299dd640c3ef2c66dcf8b34f8a5a6
hanyue1014 <wongxuanbin@gmail.com>
2024-09-12 02:03:24 +0800

    Clean up main method and add environment variables to configure gRPC host

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from db import *
 def main():
     # configure environment variables
     load_dotenv()
-    print(f"Env loaded: DB_FILE={os.getenv('DB_FILE')}; MODEL={os.getenv('MODEL')}")
+    print(f"Env loaded: DB_FILE={os.getenv('DB_FILE')};")
 
     # initialize the db
     db.connect()
@@ -26,45 +26,11 @@ def main():
     print("Initialise grpc simulation servicer")
     init_core_servicer()
 
-    testAttrs = [
-        AgentAttribute("Priority", "Scoring top marks"),
-        AgentAttribute("Subjects", "Focus on core subjects (Math, Science, English)"),
-        AgentAttribute("Budget", "Moderate-High (Willing to invest for quality tuition)"),
-    ]
-    testAttrs2 = [
-        AgentAttribute("Priority", "Scoring top marks"),
-        AgentAttribute("Subjects", "Focus on core subjects (Math, Science, English)"),
-        AgentAttribute("Budget", "Low"),
-    ]
-    testAgent = Agent(id=1, name="Bob", desc="Highly motivated student prioritizing top marks in PT3/SPM exams.", attrs=testAttrs, simulation_id=1)
-    testAgent2 = Agent(id=2, name="Bobby", desc="Highly motivated student prioritizing top marks in PT3/SPM exams. Likes to obtain suggestions from others", attrs=testAttrs2, simulation_id=1)
-    testEnv = "The Malaysian education system places high emphasis on standardized national exams like PT3 (Form 3) and SPM (Form 5) as crucial factors in determining university placement and future career opportunities. This creates a high-pressure environment for students and parents in the Klang Valley, a densely populated urban area. Here, a diverse range of tuition centers cater to various student needs and budgets, offering support for these crucial exams. However, intense competition necessitates effective marketing strategies and targeted offerings from tuition centers to attract students and their parents seeking academic success."
-    product1 = Product(
-        id=1,
-        name="Intensive Exam Preparation Course (PT3/SPM)",
-        desc="Exam-oriented teaching, Past year paper analysis, Experienced teachers. Subjects include Chinese, Moral",
-        price=350.00,  # Assuming a price in MYR
-        cost=200.00,  # Assuming a cost for the tuition center
-        simulation_id=1
-    )
-    product2 = Product(
-        id=2,
-        name="Small Group Subject Tutorials (PT3/SPM)",
-        desc="Personalized attention, Addressing individual weaknesses, Targeted practice exercises. Subjects include Maths",
-        price=180.00,  # Assuming a price in MYR
-        cost=120.00,  # Assuming a cost for the tuition center
-        simulation_id=1
-    )
-    simulation = Simulation(id=1, env_desc=testEnv, agents=[testAgent, testAgent2], products=[product1, product2], total_cycle=3)
-    simulation.init_simulation()
-    for event in simulation.run_simulation():
-        print(event, f"Agent ID: {event.agent.agent_id if event.agent is not None else -1}, Sim ID: {event.sim_id}, Cycle: {event.cycle}, Type: {event.type}, Content: {event.content}, Time: {event.time_created}")
-
 def init_core_servicer():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     marcom_core_pb2_grpc.add_MarcomServiceServicer_to_server(MarcomCoreServicer(), server)
-    server.add_insecure_port(f"[::]:{os.getenv('GRPC_CONNECTION_PORT')}")
-    print(f"Connecting to [::]:{os.getenv('GRPC_CONNECTION_PORT')}")
+    server.add_insecure_port(f"{os.getenv('GRPC_CONNECTION_HOST')}:{os.getenv('GRPC_CONNECTION_PORT')}")
+    print(f"Connecting to {os.getenv('GRPC_CONNECTION_HOST')}:{os.getenv('GRPC_CONNECTION_PORT')}")
     server.start()
     server.wait_for_termination()
 
